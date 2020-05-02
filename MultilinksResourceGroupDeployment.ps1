6bb4d8b4c9e181b582c3b61b8ad8f660a388a742
Chris Dinh <dinh.thanhtoan@gmail.com>
2020-05-02 18:54:03 +1200

    Add IdentityService to deployment script

--- MultilinksResourceGroupDeployment.ps1
+++ MultilinksResourceGroupDeployment.ps1
@@ -8,36 +8,22 @@ Function New-MultilinksResourceGroupDeployment {
 
    [CmdletBinding()]
    param (
-      [Parameter(Mandatory)] [string] $AppServicePlanName,
-      [Parameter(Mandatory)] [string] $AppServicePlanRGName,
-      [Parameter(Mandatory)] [string] $AppServicePlanRGLocation,
-      [Parameter(Mandatory)] [string] $AppServicePlanTier
-   )
-
-   $AppServicePlanParameters = @{
-      'Name'              = $AppServicePlanName
-      'ResourceGroupName' = $AppServicePlanRGName
-      'Location'          = $AppServicePlanRGLocation
-      'Tier'              = $AppServicePlanTier
-   }
-
-   New-AzureAppServicePlan @AppServicePlanParameters
-} # New-MultilinksResourceGroupDeployment
-
-Function New-MultilinksResourceGroupDeployment {
-   <#
-   .Synopsis
-      Create new Multilinks resource group deployment.
-   #>
-
-   [CmdletBinding()]
-   param (
+      [Parameter(Mandatory)] [string] $SubscriptionId,
+      [Parameter(Mandatory)] [string] $AppServiceName,
       [Parameter(Mandatory)] [string] $AppServicePlanName,
       [Parameter(Mandatory)] [string] $AppServicePlanRGName,
       [Parameter(Mandatory)] [string] $AppServicePlanRGLocation,
       [Parameter(Mandatory)] [string] $AppServicePlanTier,
-      [Parameter(Mandatory)] [string] $ResourceGroupLocation,
       [Parameter(Mandatory)] [string] $ResourceGroupName,
+      [Parameter(Mandatory)] [string] $ResourceGroupLocation,
+
+      [Parameter(Mandatory)] [string] $SqlServerName,
+      [Parameter(Mandatory)] [string] $SqlServerAdminLogin,
+      [Parameter(Mandatory)] [SecureString] $SqlServerAdminPassword,
+      [Parameter(Mandatory)] [string] $SqlDbName,
+      [Parameter(Mandatory)] [string] $SqlDbTierEdition,
+      [Parameter(Mandatory)] [string] $SqlDbTierName,
+
       [Parameter(Mandatory)] [string] $TemplateFile,
       [Parameter(Mandatory)] [string] $TemplateParametersFile
    )
@@ -62,29 +48,35 @@ Function New-MultilinksResourceGroupDeployment {
    $TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, 'ARMDeploymentFiles', $TemplateFile))
    $TemplateParametersFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, 'ARMDeploymentFiles', $TemplateParametersFile))
 
-   $MultilinksResourceGroupParameters = @{
-      'ResourceGroupLocation'  = $ResourceGroupLocation
-      'ResourceGroupName'      = $ResourceGroupName
-      'TemplateFile'           = $TemplateFile
-      'TemplateParametersFile' = $TemplateParametersFile
-   }
-
    # Create the resource group only when it doesn't already exist
-   if ($null -eq (Get-AzResourceGroup -Name $MultilinksResourceGroupParameters.ResourceGroupName -Location $MultilinksResourceGroupParameters.ResourceGroupLocation -Verbose -ErrorAction SilentlyContinue)) {
-      New-AzResourceGroup -Name $MultilinksResourceGroupParameters.ResourceGroupName -Location $MultilinksResourceGroupParameters.ResourceGroupLocation -Verbose -Force -ErrorAction Stop
+   if ($null -eq (Get-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -ErrorAction SilentlyContinue)) {
+      New-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force -ErrorAction Stop
    }
 
-   # $OptionalParameters = New-Object -TypeName Hashtable
-   # New-AzResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
-   #                                  -ResourceGroupName $ResourceGroupName `
-   #                                  -TemplateFile $TemplateFile `
-   #                                  -TemplateParameterFile $TemplateParametersFile `
-   #                                  @OptionalParameters `
-   #                                  -Force -Verbose `
-   #                                  -ErrorVariable ErrorMessages
-   # 
-   # if ($ErrorMessages) {
-   #    Write-Output '', 'Template deployment returned the following errors:', @(@($ErrorMessages) | ForEach-Object { $_.Exception.Message.TrimEnd("`r`n") })
-   # }
+   $MultilinksResourceGroupParameters = @{
+      'SubscriptionId'           = $SubscriptionId
+      'AppServiceName'           = $AppServiceName
+      'AppServicePlanName'       = $AppServicePlanName
+      'AppServicePlanRGName'     = $AppServicePlanRGName
+
+      'SqlServerName'            = $SqlServerName
+      'SqlServerAdminLogin'      = $SqlServerAdminLogin
+      'SqlServerAdminPassword'   = $SqlServerAdminPassword
+      'SqlDbName'                = $SqlDbName
+      'SqlDbTierEdition'         = $SqlDbTierEdition
+      'SqlDbTierName'            = $SqlDbTierName
+   }
+
+   New-AzResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+                                    -ResourceGroupName $ResourceGroupName `
+                                    -TemplateFile $TemplateFile `
+                                    -TemplateParameterFile $TemplateParametersFile `
+                                    @MultilinksResourceGroupParameters `
+                                    -Force -Verbose `
+                                    -ErrorVariable ErrorMessages
+   
+   if ($ErrorMessages) {
+      Write-Output '', 'Template deployment returned the following errors:', @(@($ErrorMessages) | ForEach-Object { $_.Exception.Message.TrimEnd("`r`n") })
+   }
 
 } # New-MultilinksResourceGroupDeployment
