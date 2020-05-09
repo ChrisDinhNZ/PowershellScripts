c1dfaacec6c3a4e6c0cb138901afb5e87a630fe3
Chris Dinh <dinh.thanhtoan@gmail.com>
2020-05-10 10:40:57 +1200

    Using Azure Portal to manage Key Vault for now so remove New-AzureKeyVault function

--- ChrisDinh-AzureUtilityScripts.ps1
+++ ChrisDinh-AzureUtilityScripts.ps1
@@ -89,35 +89,3 @@ Function New-AzureAppServicePlan {
 
    New-AzAppServicePlan -Name $Name -ResourceGroupName $ResourceGroupName -Location $Location -Tier $Tier
 } # New-AzureAppServicePlan
-
-Function New-AzureKeyVault {
-   <#
-   .Synopsis
-      Create new key vault under specified resource group.
-   #>
-
-   [CmdletBinding()]
-   param (
-      [Parameter(Mandatory)] [string] $Name,
-      [Parameter(Mandatory)] [string] $ResourceGroupName,
-      [Parameter(Mandatory)] [string] $ServicePrincipleObjectId,
-      [string] $Location = "southeastasia",
-      [string] $Sku = "Standard"
-   )
-
-   $KeyVault = Get-AzKeyVault -VaultName $Name
-
-   if ($null -eq $KeyVault) {
-      $ResourceGroup = Get-AzResourceGroup -Name $ResourceGroupName -Location $Location -Verbose -ErrorAction SilentlyContinue
-
-      if ($null -eq $ResourceGroup) {
-         # Create new resource group.
-         New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Verbose -Force -ErrorAction Stop
-      }
-
-      $KeyVault = New-AzKeyVault -VaultName $Name -ResourceGroupName $ResourceGroupName -Location $Location -Sku $Sku -EnabledForDiskEncryption -EnabledForTemplateDeployment -Verbose
-      Set-AzKeyVaultAccessPolicy -VaultName $Name -ResourceGroupName $ResourceGroupName -ObjectId $ServicePrincipleObjectId -PermissionsToSecrets set,get,list -Verbose
-   }
-
-   $KeyVault
-} # New-AzureKeyVault
